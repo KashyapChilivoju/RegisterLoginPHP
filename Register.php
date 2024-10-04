c74b00a55ba8e64e3661009e75dc537b6ed6db95
Kashyap Chilivoju <s3659816@student.rmit.edu.au>
2024-10-04 20:46:40 +1000

    Added Script for form validation no PHP yet

--- Register.php
+++ Register.php
@@ -1,16 +1,40 @@
 ﻿<html>
 <body>
 
-<form action="welcome.php" method="POST">
-    Name: <input type="text" name="name">
-    E-mail: <input type="text" name="email">
-    Mobile: <input type="number" name="mobile">
-    Password: <input type="password" name="password"><br><br>
+<form id="registerForm">
+    Name: <input type="text" id="name">
+    E-mail: <input type="text" id="email">
+    Mobile: <input type="number" id="mobile">
+    Password: <input type="password" id="password"><br><br>
     Gender:
-    <input type="radio" name="gender" value="male"> Male
-    <input type="radio" name="gender" value="female"> Female
+    <input type="radio" id="male" name="gender" value="male"> Male
+    <input type="radio" id="female" name="gender" value="female"> Female<br><br>
     <input type="submit">
 </form>
+
+<script>
+    // Function to capture form data and store it in localStorage
+    document.getElementById('registerForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission to server
+
+        // Capture form data
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var mobile = document.getElementById('mobile').value;
+        var password = document.getElementById('password').value;
+        var gender = document.querySelector('input[name="gender"]:checked')?.value;
+
+        // Storing the data in localStorage
+        localStorage.setItem('name', name);
+        localStorage.setItem('email', email);
+        localStorage.setItem('mobile', mobile);
+        localStorage.setItem('password', password);
+        localStorage.setItem('gender', gender);
+
+        // Optional: Display a confirmation message or redirect
+        alert('User data stored in localStorage!');
+    });
+</script>
 
 </body>
 </html>
